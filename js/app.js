ed418a99af90a9843012928c947f24f59b9df3d7
Perry <apk29@hotmail.com>
2018-08-28 23:25:44 -0700

    Added working counter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-
- * Create a list that holds all of your cards
- */
+/** Create a list that holds all of your cards*/
 let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
     "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt",
     "fa-cube", "fa-cube", "fa-anchor", "fa-anchor", "fa-leaf", "fa-leaf",
@@ -8,7 +6,8 @@ let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
 ];
 
 let openCard = [],
-    startGame = false;
+    startGame = false,
+    matchCount = 0;
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -55,16 +54,17 @@ function toggleCard() {
  *add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
 function cardMatch() {
     if (openCard.length === 2) {
-       /*if the list already has another card, check to see if the two cards match*/
-       /*path to compare the name of the cards*/
+        /*if the list already has another card, check to see if the two cards match*/
+        /*path to compare the name of the cards*/
         if (openCard[0][0].classList[2] === openCard[1][0].classList[2]) {
             /*add match so removeClass doesn't delete pairs that are already matched*/
-            openCard[0][0].classList.add("match"); 
+            openCard[0][0].classList.add("match");
             openCard[1][0].classList.add("match");
             /*if the cards do match, lock the cards in the open position 
             (put this functionality in another function that you call from this one)*/
             $(openCard[0]).off('click');
             $(openCard[1]).off('click');
+            matchCount++;
             emptyOpenCard();
             console.log(openCard);
         } else {
@@ -73,22 +73,32 @@ function cardMatch() {
             openCard[0][0].classList.add("wrong");
             openCard[1][0].classList.add("wrong");
             /*removes the "open show" so card flips over to blank again because cards did not match*/
-            setTimeout(removeClass, 900); 
-            setTimeout(emptyOpenCard, 900);
-            console.log("two")
+            setTimeout(removeClass, 1000);
+            setTimeout(emptyOpenCard, 1000);
+            matchCount++;console.log(matchCount);
+            
         }
     }
+    /*increment the move counter and display it on the page (put this functionality in 
+    another function that you call from this one)*/
+    movesCount();console.log("one");    
+}
 
+function movesCount() {
+    if (matchCount % 2 === 0 || matchCount === 1) {
+        $(".moves").text(matchCount.toString());
+        console.log("three");
+    };
 }
 
 function removeClass() {
     $('.card').removeClass("open show");
-    console.log("remove")
+    console.log("four")
 }
 
 function emptyOpenCard() {
     openCard = [];
-    console.log("emptyOpenCard")
+    console.log("five")
 }
 createCard();
 shuffle(cards);
@@ -101,14 +111,7 @@ toggleCard();
  
  *   
  *    + 
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + 
+ *    + if all cards have matched, display a message with the final score (put this 
+        functionality in another function that you call from this one)
  */
-/*const deck = document.querySelector('.deck');
-
-deck.addEventListener('click', event => {
-    const clickTarget = event.target;
-    if (clickTarget.classList.contains('card')) {
-        clickTarget.classList.toggle('open');
-        clickTarget.classList.toggle('show');
-    }
